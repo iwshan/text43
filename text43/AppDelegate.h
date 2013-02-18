0f721255c9bd8d753bf0d8caa49c864a4f972ef9
iwshan <iwshan@sina.com>
2013-02-18 10:43:34 +0800

    Initial Commit

---
+++ text43/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  text43
+//
+//  Created by wusan on 13-2-18.
+//  Copyright (c) 2013年 iwshan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
