1bb8ef7969f3eea5c6c2a220d615ae17fda05c32
iwshan <iwshan@sina.com>
2013-02-18 10:57:33 +0800

    add int a

--- text43/AppDelegate.m
+++ text43/AppDelegate.m
@@ -21,6 +21,11 @@
     self.window = [[[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]] autorelease];
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
+    
+    
+    int a;
+    
+    
     [self.window makeKeyAndVisible];
     return YES;
 }
